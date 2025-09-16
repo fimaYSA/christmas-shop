8341e6fd9efab3474dc039d4ee1e399122b5f98f
Efimenko_Sergey <fima.ysa@mail.ru>
2025-09-16 17:39:36 +0300

    feat: add section Slider

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,8 @@ export function Home() {
   return (
     <div
       className='bg-primary px-2 rounded-t-[20px] font-montserrat text-white
-                 bg-[url(./././assets/img-compressed/bg-snow.png)] bg-contain bg-repeat-y'
+                 bg-[url(./././assets/img-compressed/bg-snow.png)] bg-contain bg-repeat-y
+                 overflow-hidden'
     >
       <section className='py-15 bg-[url(./././assets/img-compressed/bg-ball.png)] bg-cover bg-center bg-no-repeat'>
         <div className='w-[426px] py-15 mx-auto flex flex-col items-center gap-3'>
@@ -36,6 +37,60 @@ export function Home() {
           </div>
           <div className='w-1/2 h-[528px] bg-[url(./././assets/img-compressed/santa.png)]
                          bg-center bg-no-repeat rounded-[20px]'></div>
+        </div>
+      </section>
+
+      <section className='py-15'>
+        <div className='max-w-[1276px] mx-auto'>
+          <div className='text-center'>
+            <p className='font-allura text-2xl tracking-wider leading-none mb-3'>Become Happier!</p>
+            <h2 className='text-2xl uppercase font-semibold tracking-widest'>in the new 2025</h2>
+          </div>
+          <div className='inline-flex gap-5 items-center my-5'>
+            <span className='font-semibold text-[80px] uppercase'>live</span>
+            <img src='../../../src/assets/img-compressed/snowman.png'
+                 alt=''
+                 className='size-50 rounded-[20px]' />
+            <span className='font-semibold text-[80px] uppercase'>create</span>
+            <img src='../../../src/assets/img-compressed/christmas-trees.png'
+                 alt=''
+                 className='size-50 rounded-[20px]' />
+            <span className='font-semibold text-[80px] uppercase'>Love</span>
+            <img src='../../../src/assets/img-compressed/christmas-tree-ball.png'
+                 alt=''
+                 className='size-50 rounded-[20px]' />
+            <span className='font-semibold text-[80px] uppercase'>dream</span>
+            <img src='../../../src/assets/img-compressed/fairytale-house.png'
+                 alt=''
+                 className='size-50 rounded-[20px]' />
+          </div>
+          <div className='flex gap-5 justify-end'>
+            <button className='size-14 border rounded-[20px] text-white/40 rotate-180'>
+              <svg className='mx-auto stroke-current'
+                   width='14'
+                   height='14'
+                   viewBox='0 0 14 14'
+                   fill='none'
+                   xmlns='http://www.w3.org/2000/svg'>
+                <path d='M1 7H13.5M13.5 7L7.5 1M13.5 7L7.5 13'
+                      stroke-linecap='round'
+                      stroke-linejoin='round' />
+              </svg>
+            </button>
+            <button className='size-14 border rounded-[20px] text-white cursor-pointer
+                              hover:text-primary hover:bg-white transition-colors duration-300'>
+              <svg className='mx-auto stroke-current'
+                   width='14'
+                   height='14'
+                   viewBox='0 0 14 14'
+                   fill='none'
+                   xmlns='http://www.w3.org/2000/svg'>
+                <path d='M1 7H13.5M13.5 7L7.5 1M13.5 7L7.5 13'
+                      stroke-linecap='round'
+                      stroke-linejoin='round' />
+              </svg>
+            </button>
+          </div>
         </div>
       </section>
     </div>
