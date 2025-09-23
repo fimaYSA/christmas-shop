46571b368f8eab976662016389228341fde9e53e
Efimenko_Sergey <fima.ysa@mail.ru>
2025-09-23 14:16:13 +0300

    refactor: burger menu

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -14,7 +14,7 @@ function BurgerMenu() {
       {/* Кнопка бургера */}
       <button
         onClick={toggleMenu}
-        className='flex flex-col gap-2 justify-center items-center size-10 cursor-pointer'
+        className=' absolute top-3 right-2 flex flex-col gap-2 justify-center items-center size-10 cursor-pointer z-2'
       >
         {/* Иконка бургера */}
         <div className='w-5 border-b-1 border-b-dark transition-all duration-300'
@@ -25,10 +25,10 @@ function BurgerMenu() {
 
       {/* Меню */}
       <div
-        className={`absolute right-0 top-16 w-full h-[100vh] bg-white inline-flex flex-col
+        className={`absolute top-0 right-0 w-full h-full bg-white flex-col inline-flex 
         justify-center gap-2 items-center font-montserrat text-2xl font-semibold uppercase tracking-[3.8px]
-        leading-9 transition duration-300 delay-150
-        ${isOpen ? 'opacity-100' : 'opacity-0'}`}
+        leading-9 transition duration-300 delay-150 -translate-y-full z-1
+        ${isOpen ? 'translate-y-0' : ''}`}
       >
         {BTN_MENU.map(b =>
           <NavHashLink to={b.link}
